cea8a7535c3e064ef8e9bb59c22d85b118df1cd7
_p4lm4 <matejadjokic23@gmail.com>
2023-02-03 14:50:16 +0100

    Big Bang

---
+++ sketch.js
@@ -0,0 +1,194 @@
+const tiles = [];
+const value = 50;
+
+let grid = [];
+
+const DIM = 64;
+
+const BLANK = 0;
+const UP = 1;
+const RIGHT = 2;
+const DOWN = 3;
+const LEFT = 4;
+
+const rules = [
+    [
+        [BLANK, UP],
+        [BLANK, RIGHT],
+        [BLANK, DOWN],
+        [BLANK, LEFT],
+    ],
+    [
+        [RIGHT, LEFT, DOWN],
+        [LEFT, UP, DOWN],
+        [BLANK, DOWN],
+        [RIGHT, UP, DOWN],
+    ],
+    [
+        [RIGHT, LEFT, DOWN],
+        [LEFT, UP, DOWN],
+        [RIGHT, LEFT, UP],
+        [BLANK, LEFT],
+    ],
+    [
+        [BLANK, UP],
+        [LEFT, UP, DOWN],
+        [RIGHT, LEFT, UP],
+        [RIGHT, UP, DOWN],
+    ],
+    [
+        [RIGHT, LEFT, DOWN],
+        [BLANK, RIGHT],
+        [RIGHT, LEFT, UP],
+        [UP, DOWN, RIGHT],
+    ],
+];
+
+function preload() {
+    tiles[0] = loadImage("tiles/demo/blank.png");
+    tiles[1] = loadImage("tiles/demo/up.png");
+    tiles[2] = loadImage("tiles/demo/right.png");
+    tiles[3] = loadImage("tiles/demo/down.png");
+    tiles[4] = loadImage("tiles/demo/left.png");
+}
+
+function setup() {
+    createCanvas(800, 800);
+
+    for (let i = 0; i < DIM * DIM; i++) {
+        grid[i] = {
+            collapsed: false,
+            options: [
+                BLANK,
+                UP,
+                RIGHT,
+                DOWN,
+                LEFT,
+            ]
+        };
+    }
+}
+
+function checkValid(arr, valid) {
+    for (let i = arr.lenght - 1; i >= 0; i--) {
+        let element = arr[i];
+        if (!valid.includes(element)) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+function mousePressed() {
+    redraw();
+}
+
+const iter = 6;
+
+function draw() {
+    for (let i = 0; i < iter; i++) {
+        background(0);
+
+        const w = width / DIM;
+        const h = height / DIM;
+        for (let j = 0; j < DIM; j++) {
+            for (let i = 0; i < DIM; i++) {
+                let cell = grid[i + j * DIM];
+                if (cell.collapsed) {
+                    let index = cell.options[0];
+                    image(tiles[index], i * w, j * h, w, h);
+                } else {
+                    fill(0);
+                    stroke(255);
+                    rect(i * w, j * h, w, h);
+                }
+            }
+        }
+
+        let gridCopy = grid.slice();
+        gridCopy = gridCopy.filter((a) => !a.collapsed);
+
+        if (gridCopy.length == 0) {
+            return;
+        }
+
+        gridCopy.sort((a, b) => {
+            return a.options.length - b.options.length;
+        });
+
+        let len = gridCopy[0].options.length;
+        let stopIndex = 0;
+        for (let i = 0; i < gridCopy.length; i++) {
+            if (gridCopy[i].options.length > len) {
+                stopIndex = i;
+                break;
+            }
+        }
+        if (stopIndex > 0) {
+            gridCopy.splice(stopIndex);
+        }
+        const cell = random(gridCopy);
+        cell.collapsed = true;
+        const pick = random(cell.options);
+        cell.options = [pick];
+
+
+        const nextGrid = [];
+        for (let j = 0; j < DIM; j++) {
+            for (let i = 0; i < DIM; i++) {
+                let index = i + j * DIM;
+                if (grid[index].collapsed) {
+                    nextGrid[index] = grid[index];
+                } else {
+                    let options = [BLANK, UP, RIGHT, DOWN, LEFT];
+                    // look up
+                    if (j > 0) {
+                        let up = grid[i + (j - 1) * DIM];
+                        let validOptions = [];
+                        for (let option of up.options) {
+                            let valid = rules[option][2];
+                            validOptions = validOptions.concat(valid);
+                        }
+                        checkValid(options, validOptions);
+                    };
+                    // look right
+                    if (i < DIM - 1) {
+                        let right = grid[i + 1 + j * DIM];
+                        let validOptions = [];
+                        for (let option of right.options) {
+                            let valid = rules[option][3];
+                            validOptions = validOptions.concat(valid);
+                        }
+                        checkValid(options, validOptions);
+                    };
+                    // look down
+                    if (j < DIM - 1) {
+                        let down = grid[i + (j + 1) * DIM];
+                        let validOptions = [];
+                        for (let option of down.options) {
+                            let valid = rules[option][0];
+                            validOptions = validOptions.concat(valid);
+                        }
+                        checkValid(options, validOptions);
+                    };
+                    // look left
+                    if (i > 0) {
+                        let left = grid[i - 1 + j * DIM];
+                        let validOptions = [];
+                        for (let option of left.options) {
+                            let valid = rules[option][1];
+                            validOptions = validOptions.concat(valid);
+                        }
+                        checkValid(options, validOptions);
+                    };
+
+                    nextGrid[index] = {
+                        options,
+                        collapsed: false,
+                    };
+                }
+            }
+        }
+
+        grid = nextGrid;
+    }
+}
